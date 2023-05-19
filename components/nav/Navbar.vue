0cb952c0d6806fb54cfa9867c305dc5a81d52a70
Hiromichi NOMATA <git@hiromichinomata.com>
2023-05-19 20:54:24 +0900

    update: NuxtLink

--- components/nav/Navbar.vue
+++ components/nav/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
   </nav>
 </template>
